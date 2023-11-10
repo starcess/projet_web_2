e993c1458a5b4153e32829a01f134875eaa7195c
Rebecca Dauphnie Mombrun <joceline1233@gmail.com>
2023-11-10 14:23:00 -0500

    Update UnProduit.php

--- Projet2_web/Views/UnProduit.php
+++ Projet2_web/Views/UnProduit.php
@@ -12,7 +12,32 @@
     <?php include('header.php'); ?>
 
     <div class="body_container">
+        <div class="img-container">
+            <script>
+                fetch
+            </script>
+        </div>
+        <div class="conteneurTxt">
+            <div class="conteneurDescription">
+                <script>
+                    //url =http://localhost:4208/projet_web_2/Projet2_web/Views/unProduit.php
+                    fetch
+                </script>
+            </div>
+            <div class="conteneurPrix">
+                <script>
+                    fetch
+                </script>
+            </div>
 
+        </div>
+        <div class="autreProduit">
+            <div class="autreProduit">
+                <script>
+                    fetch
+                </script>
+            </div>
+        </div>
     </div>
 
 </body>
